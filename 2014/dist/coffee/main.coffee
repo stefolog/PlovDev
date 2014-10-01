f47d24966c966e437c41a6532845f2cd097e69d7
Stef <stefan@getti.org>
2014-10-01 22:39:54 +0300

    Latest framework version recovered from the server

---
+++ 2014/dist/coffee/main.coffee
@@ -0,0 +1,90 @@
+_.templateSettings.variable = "ctx"
+
+ProgramSlot = Backbone.Model.extend
+  defaults:
+    start: ''
+    title: ''
+    icon: ''
+    speaker:
+      id: ''
+      name: ''
+
+Speaker = Backbone.Model.extend
+  defaults:
+    id: ''
+    name: ''
+    img: ''
+    personalPage: ''
+    resume: ''
+    company:
+      name: ''
+      url: ''
+
+
+ProgramSlotView = Backbone.View.extend {
+  tagName: 'tr'
+
+  initialize: () ->
+    _.bindAll(this, 'render')
+    return
+
+  render: () ->
+    template = _.template $('#program_slot_template').html()
+    $(this.el).append(
+      template
+        slot: this.model
+    )
+    return this.el
+}
+
+
+SpeakerView = Backbone.View.extend {
+  tagName: 'div'
+
+  initialize: () ->
+    _.bindAll(this, 'render')
+    return
+
+  render: () ->
+    template = _.template $('#speaker_template').html()
+
+    $(this.el).addClass("lector");
+    $(this.el).attr('id', this.model.get('id'))
+
+    $(this.el).html(
+      template
+        speaker: this.model
+    )
+
+    return this.el
+}
+
+
+Program = Backbone.Collection.extend
+  model: ProgramSlot
+  url: './data/program.json'
+
+Speakers = Backbone.Collection.extend
+  model: Speaker
+  url: './data/speakers.json'
+
+
+$( () ->
+  program = new Program()
+  program.fetch
+    success: (collection) ->
+      unless collection.models.length
+        $('#agenda').detach()
+        $('#nav-agenda').detach()
+
+      for slot in collection.models
+        slotView = new ProgramSlotView( { model: slot })
+        $('#agenda-table table').append(slotView.render())
+
+  speakers = new Speakers()
+  speakers.fetch
+    success: (collection) ->
+      for speaker in collection.models
+        speaker = new SpeakerView( { model: speaker })
+        $('#lectors-info').append(speaker.render())
+)
--- 2014/dist/js/scripts.js
+++ 2014/dist/js/scripts.js
@@ -7,7 +7,7 @@ $('<img/>').attr('src', 'dist/images/background.jpg').load(function() {
 	jQuery('#logo').fadeIn(600, function() {
 		jQuery('h1').animate({opacity: '1', 'padding-top': '0'}, 500,function() {
 			jQuery('h2').animate({opacity: '1', 'padding-top': '0'}, 500,function() {
-				if(jQuery(window).width()<767){	
+				if(jQuery(window).width()<767){
 					jQuery('#explore').animate({opacity: '1', 'margin-top': '2em'}, 1000);
 				} else {
 					jQuery('#explore').animate({opacity: '1', 'margin-top': '4em'}, 1000);
@@ -17,36 +17,31 @@ $('<img/>').attr('src', 'dist/images/background.jpg').load(function() {
 	});
 });
 
-textCenter();
-
-	function textCenter()
-{
-	$('.text-container').css({
+function textCenter() {
+  	$('.text-container').css({
         position:'absolute'
     });
 
     $('.text-container').css({
- 
         left: ($(window).width() - $('.text-container').outerWidth())/2,
         top: ($(window).height() - $('.text-container').outerHeight())/2
-
     });
-
 }
-	
-jQuery(document).ready(function() {
+
+$(textCenter);
+$(function() {
 
 	/*-----------------------------------------------------------------------------------*/
 	/*	Navigation
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	var animate='down';
-	
+
 	jQuery(window).bind('scroll', function () {
-	
+
 		/* Animation for Top Navigation */
 		var scrollTop = jQuery(window).scrollTop();
-		
+
 		if (scrollTop > jQuery('#info').offset().top-60 && animate == 'down') {
 			animate='up';
 			jQuery('#top-bar').stop().animate({top:'0'}, 300);
@@ -54,7 +49,7 @@ jQuery(document).ready(function() {
 			animate='down';
 			jQuery('#top-bar').stop().animate({top:'-75px'}, 300);
 		}
-		
+
 		/* Update Section on Top-Bar */
 		jQuery('section').each(function(){
 			if (scrollTop > jQuery(this).offset().top-60){
@@ -68,81 +63,81 @@ jQuery(document).ready(function() {
 			}
 		});
 	});
-	
+
 	/* Responsive Menu Click */
 	jQuery('#menu-mobile').click(function(){
 		if ( jQuery("#top-navigation ul").is(":visible") ) {
 		    jQuery("#top-navigation ul").slideUp(500);
 		   	jQuery('#menu-mobile').removeClass('active');
-		} else { 
+		} else {
 		   	jQuery("#top-navigation ul").slideDown(500);
 		   	jQuery('#menu-mobile').addClass('active');
 		}
 	});
-	
+
 	/* On Resize show menu on desktop if hidden */
 	jQuery(window).resize(function() {
 		textCenter();
-	    if(jQuery(window).width()>992){	
+	    if(jQuery(window).width()>992){
 			if (jQuery("#top-navigation ul").is(":hidden") ) {
 			    jQuery("#top-navigation ul").show();
-			   	jQuery('#menu-mobile').removeClass('active');		
+			   	jQuery('#menu-mobile').removeClass('active');
 			}
 	    } else {
 	    	if (jQuery("#top-navigation ul").is(":visible") ) {
 			    jQuery("#top-navigation ul").hide();
-			   	jQuery('#menu-mobile').removeClass('active');		
+			   	jQuery('#menu-mobile').removeClass('active');
 			}
 
 
 	    }
 	});
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/*	Smooth Scroll - Navigation + .scroll items
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	jQuery('#top-navigation li').bind('click',function(event){
 	    var anchor = jQuery(this).find('a');
-	    
+
 	    jQuery('#top-navigation li').removeClass('active');
 	    jQuery(this).addClass('active');
-	
+
 	    jQuery('html, body').stop().animate({
 	        scrollTop: jQuery(anchor.attr('href')).offset().top-50
 	    }, 1500,'easeInOutExpo');
-	    
+
 	    /* If Mobile hide menu on select */
-	    if(jQuery(window).width()<=767){	
+	    if(jQuery(window).width()<=767){
 		    jQuery("#top-navigation ul").slideUp(500);
 			jQuery('#menu-mobile').removeClass('active');
 	    }
-	    
+
 	    event.preventDefault();
 	});
-	
+
 	jQuery('.scroll').bind('click',function(event){
 	    var anchor = jQuery(this);
-	
+
 	    jQuery('html, body').stop().animate({
 	        scrollTop: jQuery(anchor.attr('href')).offset().top-50
 	    }, 1500,'easeInOutExpo');
-	    
+
 	    /* If Mobile hide menu on select */
-	    if(jQuery(window).width()<=767){	
+	    if(jQuery(window).width()<=767){
 		    jQuery("#top-navigation ul").slideUp(500);
 			jQuery('#menu-mobile').removeClass('active');
 	    }
-	    
+
 	    event.preventDefault();
 	});
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/*	Services
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	var curSkills="branding";
-	
+
 	/* Services Animations */
 	jQuery('.service').click(function(){
 		jQuery('.service').removeClass('active');
@@ -153,10 +148,10 @@ jQuery(document).ready(function() {
 		jQuery('html, body').stop().animate({
 		    scrollTop: jQuery('#services-list').offset().top-100
 		}, 1000,'easeInOutExpo');
-		
+
 		jQuery("#"+target+"-skills .expand").each(function() {
 			jQuery(this).css({width:0}).animate({width:jQuery(this).attr('data-width')}, 2000);
 		});
 	});
-	
+
 });
